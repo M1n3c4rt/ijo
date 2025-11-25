4b788ce8594a300788f69c0246d2f494d947ab8a
m1n3c4rt <vedicbits@gmail.com>
2025-11-25 18:46:51 +0530

    first commit

---
+++ exe/Main.hs
@@ -0,0 +1,113 @@
+--{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+module Main where
+import Data.Char (ord)
+import Data.List.Extra (splitOn)
+import qualified Data.HashMap.Strict as HM
+import System.Environment (getArgs)
+
+newtype Program = Program (HM.HashMap Char Function)
+data Function = Fun {arg :: Char, bias :: Arrow, body :: Exp}
+data Arrow = LeftA | RightA
+data Exp = Unit | Sum Exp Exp | Product Exp Exp | Appl Char Exp | Var Char deriving Show
+
+data Value = U | R Value | L Value | P Value Value deriving (Show, Read)
+
+-- PARSING
+
+isKanji :: Char -> Bool
+isKanji c = all ($ ord c) [(>=19968),(<=40959)]
+
+isGreek :: Char -> Bool
+isGreek c = all ($ ord c) [(>=945),(<=969)]
+
+parseExp :: String -> Exp
+parseExp e
+    | null e = error "expected token"
+    | head e == '⟨' && last e == '⟩' = parseExp (tail $ init e)
+    | otherwise = case splitTermsOn '⊕' 0 [] e of
+        [] -> error "syntax error"
+        [e1] -> case splitTermsOn '×' 0 [] e1 of
+            [] -> error "syntax error"
+            [e2] -> case splitTermsOn '＄' 0 [] e2 of
+                [] -> error "syntax error"
+                [e3] -> case e3 of
+                    "◯" -> Unit
+                    [g] -> if isGreek g then Var g else error "expected lowercase greek letter"
+                    _ -> error "syntax error"
+                [a,b] -> case a of
+                    [k] -> if isKanji k then Appl k (parseExp b) else error "expected kanji"
+                    _ -> error "expected kanji"
+                _ -> error "expected one argument in function application"
+            ls -> foldr2 Product $ map parseExp ls
+        ls -> foldr2 Sum $ map parseExp ls
+    where
+        splitTermsOn :: Char -> Int -> String -> String -> [String]
+        splitTermsOn c prec run (r:remaining) = case r of
+            '⟨' -> splitTermsOn c (prec+1) (r:run) remaining
+            '⟩' -> splitTermsOn c (prec-1) (r:run) remaining
+            _ -> if r /= c then splitTermsOn c prec (r:run) remaining else
+                    if prec == 0 then reverse run : splitTermsOn c prec [] remaining else splitTermsOn c prec (r:run) remaining
+        splitTermsOn _ prec run [] = if prec /= 0 then error "mismatched brackets" else [reverse run]
+
+        foldr2 :: (t -> t -> t) -> [t] -> t
+        foldr2 f [a,b] = f a b
+        foldr2 f (x:xs) = f x (foldr2 f xs)
+        foldr2 _ _ = error "what"
+
+parseArrow :: Char -> Arrow
+parseArrow c = case c of
+    '⇀' -> LeftA
+    '⇁' -> RightA
+    _ -> error "expected arrow"
+
+parseFunction :: String -> (Char,Function)
+parseFunction (a:b:c:s)
+    | not (isKanji a) = error "expected kanji"
+    | not (isGreek b) = error "expected lowercase greek letter"
+    | otherwise = (a,Fun {arg=b, bias=parseArrow c, body=parseExp s})
+parseFunction _ = error "invalid function syntax"
+
+parseProgram :: String -> Program
+parseProgram = Program . HM.fromList . map parseFunction . splitOn ";"
+
+-- EVALUATION
+
+coerce :: HM.HashMap Char Function -> Exp -> Arrow -> Value -> Exp -> Value
+coerce fns _ _ v (Appl f x) = let fn = HM.lookupDefault (error (f:" not defined")) f fns in coerce fns x (bias fn) v (body fn)
+coerce fns var arrow v (Var _) = coerce fns var arrow v var
+
+coerce _ _ _ _ Unit = U
+
+coerce fns var arr (L x) (Sum a _) = L $ coerce fns var arr x a
+coerce fns var arr (R x) (Sum _ b) = R $ coerce fns var arr x b
+
+coerce fns var arr (P a b) (Product c d) = P (coerce fns var arr a c) (coerce fns var arr b d)
+
+coerce fns var LeftA (P a _) x = coerce fns var LeftA a x
+coerce fns var RightA (P _ b) x = coerce fns var LeftA b x
+
+coerce fns var LeftA x (Sum a _) = L $ coerce fns var LeftA x a
+coerce fns var RightA x (Sum _ b) = R $ coerce fns var LeftA x b
+
+coerce fns var arr (L x) (Product a b) = L $ coerce fns var arr x (Product a b)
+coerce fns var arr (R x) (Product a b) = R $ coerce fns var arr x (Product a b)
+
+coerce fns var arr U (Product a b) = P (coerce fns var arr U a) (coerce fns var arr U b)
+
+toExp :: Value -> Exp
+toExp U = Unit
+toExp (L a) = Sum (toExp a) Unit
+toExp (R b) = Sum Unit (toExp b)
+toExp (P a b) = Product (toExp a) (toExp b) 
+
+main :: IO ()
+main = do
+    args <- getArgs
+    case args of
+        [a,b] -> do
+            contents <- readFile a
+            let Program fns = parseProgram contents
+                m = HM.lookupDefault (error "主 not defined") '主' fns
+            print $ coerce fns (toExp $ read b) (bias m) (read b) (body m)
+        _ -> putStrLn "usage: ijo [filename] [input]"
+    return ()
---
+++ ijo_docs.md
@@ -0,0 +1,59 @@
+◯ Documentation
+
+# TYPES
+A type can be one of the following:
+- The unit                  ◯
+- A sum of two types        α⊕β
+- A product of two types    α×β
+
+Note that × has a higher precedence than ⊕, similar to * and + in regular arithmetic. To control precedence, wrap expressions in chevron brackets : ⟨⟩
+The fact that × distributes over ⊕ is not taken advantage of, and expressions are left unsimplified according to how the user has defined them.
+
+# TYPE FUNCTIONS
+A type function accepts types and other type functions in order to return a type. A type function is defined entirely in one line. Lines are separated by Greek Question Mark (U+037E) and no newlines.
+
+A type function is named with a single kanji character (Unicode range U+4E00 to U+9FFF).
+This is followed by the function's definition.
+The definition consists of the function's argument separated by an arrow (covered later), with the return type after the arrow. The argument must be a type, represented with a single lowercase greek letter (Unicode range U+03B1 to U+03C9).
+
+Type function application is simply the function and the types separated by a Fullwidth Dollar Sign (U+FF04). It has a higher precedence than both ⊕ and ×. For example,
+
+例＄α
+
+passes the value α to the function 例.
+
+Here is a recursively defined list type:
+
+群α⇁◯⊕α×群＄α
+
+# ARROWS
+The arrows in the definition of the function type can be either ⇀ or ⇁.
+Whenever applicable, ⇀ is biased towards the left halves of the product and sum types that come before and after it respectively. Similarly, ⇁ is biased towards the right halves of the product and sum types that come before and after it respectively.
+
+# MAIN
+A valid ◯ program defines a single type function called 主. It has the same syntax as other type functions.
+
+One argument is provided in the command line.
+When the program is run, a decimal number (interpreted as a binary string) passed as the command line argument is coerced into the type described by the function body using a specific series of steps (described in the next section), and then the final value is displayed to the user.
+
+# COERCION
+The heart of ◯ is the fact that _a function can be infered entirely from its type._ The behaviour for all possible combinations of two types are given below:
+
+◯   ⇀ ◯       Trivial.
+◯   ⇀ γ⊕δ     Coerces ◯ to γ.
+◯   ⇀ γ×δ     Coerces ◯ to γ and ◯ to δ, and puts them in a pair.
+α⊕β ⇀ ◯       Trivial.
+α⊕β ⇀ γ⊕δ     Coerces α to γ or β to δ.
+α⊕β ⇀ γ×δ     Coerces α to γ×δ or β to γ×δ.
+α×β ⇀ ◯       Trivial.
+α×β ⇀ γ⊕δ     Coerces α to γ⊕δ.
+α×β ⇀ γ×δ     Coerces α to γ and β to δ.
+◯   ⇁ ◯       Trivial.
+◯   ⇁ γ⊕δ     Coerces ◯ to δ.
+◯   ⇁ γ×δ     Coerces ◯ to γ and ◯ to δ, and puts them in a pair.
+α⊕β ⇁ ◯       Trivial.
+α⊕β ⇁ γ⊕δ     Coerces α to γ or β to δ.
+α⊕β ⇁ γ×δ     Coerces α to γ×δ or β to γ×δ.
+α×β ⇁ ◯       Trivial.
+α×β ⇁ γ⊕δ     Coerces β to γ⊕δ.
+α×β ⇁ γ×δ     Coerces α to γ and β to δ.
